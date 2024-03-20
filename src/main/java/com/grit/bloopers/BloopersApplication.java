b51e6c25798ff64a29767f2cce833fc8a3646962
munhyok <mhkang990510@naver.com>
2024-03-21 05:37:20 +0900

    Add: spring boot & mybatis

---
+++ src/main/java/com/grit/bloopers/BloopersApplication.java
@@ -0,0 +1,13 @@
+package com.grit.bloopers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloopersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloopersApplication.class, args);
+	}
+
+}
